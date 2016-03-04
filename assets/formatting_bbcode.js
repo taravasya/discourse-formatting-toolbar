6f7d7ffe704947263d580a6722f3f65b94999cfc
iunctis <steven.abgrall@gmail.com>
2016-03-05 00:28:02 +0100

    Create formatting_bbcode.js

---
+++ assets/formatting_bbcode.js
@@ -0,0 +1,16 @@
+// [LEFT]...[/LEFT]
+// [CENTER]...[/CENTER]
+// [RIGHT]...[/RIGHT]
+// [JUSTIFY]...[/JUSTIFY]
+["left", "center", "right", "justify"].forEach(function(direction){
+  Discourse.BBCode.replaceBBCode(direction, function(contents) { return ['div', {'style': "text-align:" + direction}].concat(contents); });
+});
+Discourse.Markdown.whiteListTag('div', 'style', /^text-align:.+$/);
+
+
+// FLOAT LEFT AND RIGHT
+Discourse.BBCode.replaceBBCode("floatl", function(contents) { return ['div', {'class': 'floatl'}].concat(contents); });
+Discourse.Markdown.whiteListTag('div', 'class', 'floatl');
+
+Discourse.BBCode.replaceBBCode("floatr", function(contents) { return ['div', {'class': 'floatr'}].concat(contents); });
+Discourse.Markdown.whiteListTag('div', 'class', 'floatr');
