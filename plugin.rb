34e55b58ff02b825420db4fbee70cebce756d750
iunctis <steven.abgrall@gmail.com>
2016-03-05 00:43:32 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -4,4 +4,5 @@
 # authors: Steven, iunctis.fr - Thanks to ZogStrip and cpradio
 # url: https://github.com/iunctis/discourse-formatting-toolbar.git
 
+register_asset "javascripts/formatting_bbcode.js", :server_side
 enabled_site_setting :formattingtlb_ui_enabled
