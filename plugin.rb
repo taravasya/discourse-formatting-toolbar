f80b833eddd014a8088a157dcc3f8013d0a559f0
iunctis <steven.abgrall@gmail.com>
2016-03-05 00:20:12 +0100

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-formatting-toolbar
+# about: Add formatting options on your post
+# version: 1.0
+# authors: Steven, iunctis.fr - Thanks to ZogStrip and cpradio
+# url: https://github.com/iunctis/discourse-formatting-toolbar.git
+
+enabled_site_setting :formatting_ui_enabled
