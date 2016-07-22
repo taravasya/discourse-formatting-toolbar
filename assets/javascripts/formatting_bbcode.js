9914913abd6df2e33bfa8461fd6d781c54e5eece
iunctis <steven.abgrall@gmail.com>
2016-07-23 01:47:16 +0200

    Update formatting_bbcode.js

--- assets/javascripts/formatting_bbcode.js
+++ assets/javascripts/formatting_bbcode.js
@@ -1,21 +1,43 @@
 (function() {
   if (Discourse.dialect_deprecated) { return; }
 
-// [LEFT]...[/LEFT]
-// [CENTER]...[/CENTER]
-// [RIGHT]...[/RIGHT]
-// [JUSTIFY]...[/JUSTIFY]
-["left", "center", "right", "justify"].forEach(function(direction){
-  Discourse.BBCode.replaceBBCode(direction, function(contents) { return ['div', {'style': "text-align:" + direction}].concat(contents); });
+  Discourse.BBCode.replaceBBCode("floatl", function(contents) { return ['div', {'class': 'floatl'}].concat(contents); });
+  Discourse.Markdown.whiteListTag('div', 'class', 'floatl');
+
+  Discourse.BBCode.replaceBBCode("floatr", function(contents) { return ['div', {'class': 'floatr'}].concat(contents); });
+  Discourse.Markdown.whiteListTag('div', 'class', 'floatr');
+
+  // [LEFT]...[/LEFT]
+  // [CENTER]...[/CENTER]
+  // [RIGHT]...[/RIGHT]
+  // [JUSTIFY]...[/JUSTIFY]
+  ["left", "center", "right", "justify"].forEach(function(direction){
+    Discourse.BBCode.replaceBBCode(direction, function(contents) { return ['div', {'style': "text-align:" + direction}].concat(contents); });
   });
-Discourse.Markdown.whiteListTag('div', 'style', /^text-align:.+$/);
+  Discourse.Markdown.whiteListTag('div', 'style', /^text-align:.+$/);
 
+  //------------------------------- FONT -----------------------------------------
 
-// FLOAT LEFT AND RIGHT
-Discourse.BBCode.replaceBBCode("floatl", function(contents) { return ['div', {'class': 'floatl'}].concat(contents); });
-Discourse.Markdown.whiteListTag('div', 'class', 'floatl');
+  // NOTE: the regex is based on http://blog.stevenlevithan.com/archives/reverse-recursive-pattern
+  //       it's removing the most nested first and iterating using the while loop to remove less nested matches.
 
-Discourse.BBCode.replaceBBCode("floatr", function(contents) { return ['div', {'class': 'floatr'}].concat(contents); });
-Discourse.Markdown.whiteListTag('div', 'class', 'floatr');
+  function replaceFontColor (text) {
+    while (text != (text = text.replace(/\[color=([^\]]+)\]((?:(?!\[color=[^\]]+\]|\[\/color\])[\S\s])*)\[\/color\]/ig, function (match, p1, p2, offset, string) {
+      return "<font color='" + p1 + "'>" + p2 + "</font>";
+    })));
+    return text;
+  }
 
+  function replaceFontSize (text) {
+    while (text != (text = text.replace(/\[size=([^\]]+)\]((?:(?!\[size=[^\]]+\]|\[\/size\])[\S\s])*)\[\/size\]/ig, function (match, p1, p2, offset, string) {
+      return "<font size='" + p1 + "'>" + p2 + "</font>";
+    })));
+    return text;
+  }
+
+  Discourse.Dialect.addPreProcessor(replaceFontColor);
+  Discourse.Dialect.addPreProcessor(replaceFontSize);
+
+  Discourse.Markdown.whiteListTag('font', 'color');
+  Discourse.Markdown.whiteListTag('font', 'size');
 })();
