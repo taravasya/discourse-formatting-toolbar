908e69f2e86e9d22d429d5114b5bb57d9bafcc9c
iunctis <steven.abgrall@gmail.com>
2016-07-23 02:09:45 +0200

    Version 2.0
    Added color bbcode !

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-formatting-toolbar
 # about: Add formatting options on your post
-# version: 1.0
-# authors: Steven, iunctis.fr - Thanks to ZogStrip and cpradio
+# version: 2.0
+# authors: Steven, iunctis.fr - Thanks to ZogStrip, eviltrout and cpradio
 # url: https://github.com/iunctis/discourse-formatting-toolbar.git
 
 enabled_site_setting :formattingtlb_ui_enabled
