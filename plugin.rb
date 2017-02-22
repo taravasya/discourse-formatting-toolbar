bf236b2b7ac186c8293dd3e62c13f3210bc869a8
iunctis <steven.abgrall@gmail.com>
2017-02-23 00:10:43 +0100

    Revert "Version 2.1"
    This reverts commit ce29d2e970bec30849858e815364ee17667074a9.

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-formatting-toolbar
 # about: Add formatting options on your post (Discourse)
-# version: 2.1
+# version: 2.0
 # authors: Steven, iunctis.fr - Thanks to ZogStrip, eviltrout and cpradio
 # url: https://github.com/iunctis/discourse-formatting-toolbar.git
 
