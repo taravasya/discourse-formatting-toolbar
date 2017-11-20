af71a16a71c6afcce9f6472c08eaa53e6c7cfafa
iunctis <steven.abgrall@gmail.com>
2017-11-20 17:45:45 +0100

    Version 3.0
    You can activate/deactivate each button, you can have a toolbar at your
    taste

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,17 @@
 # name: discourse-formatting-toolbar
 # about: Add formatting options on your post (Discourse)
-# version: 2.3
+# version: 3.0
 # authors: Steven, iunctis.fr - Thanks to ZogStrip, eviltrout, cpradio and Sam Saffron
 # url: https://github.com/iunctis/discourse-formatting-toolbar.git
 
-enabled_site_setting :formattingtlb_ui_enabled
+enabled_site_setting :formattingtlb_ui
+enabled_site_setting :formattingtlb_underline
+enabled_site_setting :formattingtlb_addimg
+enabled_site_setting :formattingtlb_floatl
+enabled_site_setting :formattingtlb_left
+enabled_site_setting :formattingtlb_center
+enabled_site_setting :formattingtlb_right
+enabled_site_setting :formattingtlb_justify
+enabled_site_setting :formattingtlb_color
 
 register_asset 'stylesheets/formatting.scss'
