06e97cf5ede4d92e91fa9a2d20ff5f5134e76a17
iunctis <steven.abgrall@gmail.com>
2018-11-29 00:48:51 +0100

    Compatibility FA5
    Let's try this

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-formatting-toolbar
 # about: Add formatting options on your post (Discourse)
-# version: 3.1
+# version: 3.2
 # authors: Steven, iunctis.fr - Thanks to ZogStrip, eviltrout, cpradio and Sam Saffron
 # url: https://github.com/iunctis/discourse-formatting-toolbar.git
 
@@ -16,3 +16,12 @@ enabled_site_setting :formattingtlb_color
 enabled_site_setting :formattingtlb_size
 
 register_asset 'stylesheets/formatting.scss'
+
+register_svg_icon "fa-underline" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-indent" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-left" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-center" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-right" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-align-justify" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-palette" if respond_to?(:register_svg_icon)
+register_svg_icon "fa-font" if respond_to?(:register_svg_icon)
