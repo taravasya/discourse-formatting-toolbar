f4d4d30badc8e9d5a439e8bbde82623ccf8ed1c3
iunctis <steven.abgrall@gmail.com>
2019-11-06 22:41:46 +0100

    Delete the multiple options for now

--- plugin.rb
+++ plugin.rb
@@ -5,15 +5,6 @@
 # url: https://github.com/iunctis/discourse-formatting-toolbar.git
 
 enabled_site_setting :formattingtlb_enabled
-enabled_site_setting :formattingtlb_underline
-enabled_site_setting :formattingtlb_addimg
-enabled_site_setting :formattingtlb_floatl
-enabled_site_setting :formattingtlb_left
-enabled_site_setting :formattingtlb_center
-enabled_site_setting :formattingtlb_right
-enabled_site_setting :formattingtlb_justify
-enabled_site_setting :formattingtlb_color
-enabled_site_setting :formattingtlb_size
 
 register_asset 'stylesheets/formatting.scss'
 
